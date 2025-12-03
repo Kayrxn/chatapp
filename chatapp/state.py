993bcb5ee3b2be091def4a6a309def86ebc13c35
Kayrxn <xharukax99@gmail.com>
2025-12-03 14:13:07 +0100

    feat: Añadir streaming al chat conforme es generado

--- chatapp/state.py
+++ chatapp/state.py
@@ -19,3 +19,25 @@ class State(rx.State):
         self.question = ""
 
 
+import asyncio
+
+
+async def answer(self):
+    # Our chatbot is not very smart right now...
+    answer = "I don't know!"
+    self.chat_history.append((self.question, ""))
+
+    # Clear the question input.
+    self.question = ""
+    # Yield here to clear the frontend input before continuing.
+    yield
+
+    for i in range(len(answer)):
+        # Pause to show the streaming effect.
+        await asyncio.sleep(0.1)
+        # Add one letter at a time to the output.
+        self.chat_history[-1] = (
+            self.chat_history[-1][0],
+            answer[: i + 1],
+        )
+        yield

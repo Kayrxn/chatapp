7aa41bdebaab8dec86b1ed385fa5aed867d3a855
Kayrxn <xharukax99@gmail.com>
2025-12-03 13:11:44 +0100

    feat: Añadir funciones para dos preguntas

--- chatapp/chatapp.py
+++ chatapp/chatapp.py
@@ -1,36 +1,40 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+# chatapp.py
 
 import reflex as rx
 
-from rxconfig import config
 
-
-class State(rx.State):
-    """The app state."""
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(question, text_align="right"),
+        rx.box(answer, text_align="left"),
+        margin_y="1em",
     )
 
 
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
+        ),
+        (
+            "What can I make with it?",
+            "Anything from a simple website to a complex web app!",
+        ),
+    ]
+    return rx.box(
+        *[
+            qa(question, answer)
+            for question, answer in qa_pairs
+        ]
+    )
+
+
+def index() -> rx.Component:
+    return rx.container(chat())
+
+
+
+# Add state and page to the app.
 app = rx.App()
 app.add_page(index)

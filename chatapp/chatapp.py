90819939756f0ce380b5b50764a5503cedf02e85
Kayrxn <xharukax99@gmail.com>
2025-12-03 13:17:23 +0100

    feat: Adjuntar los estilos al módulo principal

--- chatapp/chatapp.py
+++ chatapp/chatapp.py
@@ -1,13 +1,21 @@
 # chatapp.py
-
 import reflex as rx
+
+from chatapp import style
 
 
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="right"),
-        rx.box(answer, text_align="left"),
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
         margin_y="1em",
+        width="100%",
     )
 
 
@@ -32,21 +40,23 @@ def chat() -> rx.Component:
 
 def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Ask a question"),
-        rx.button("Ask"),
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button("Ask", style=style.button_style),
     )
 
 
 def index() -> rx.Component:
-    return rx.container(
-        chat(),
-        action_bar(),
+    return rx.center(
+        rx.vstack(
+            chat(),
+            action_bar(),
+            align="center",
+        )
     )
 
 
-
-
-
-# Add state and page to the app.
 app = rx.App()
 app.add_page(index)
